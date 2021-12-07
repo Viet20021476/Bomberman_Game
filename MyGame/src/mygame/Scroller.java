92ec2a95663a5119077ccf6e9bfe6ba7b761be2d
Viet20021476 <78094729+Viet20021476@users.noreply.github.com>
2021-12-07 21:57:56 +0700

    Update Scroller.java

--- MyGame/src/mygame/Scroller.java
+++ MyGame/src/mygame/Scroller.java
@@ -4,26 +4,27 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 public class Scroller {
+
     private final int BASE_X = 0;
     private final int BASE_Y = GamePanel.TILESIZE;
-    
+
     private GamePanel gamePanel;
     private int offsetX = 0;
     private int offsetY = 0;
-    
+
     public Scroller(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
     }
-    
+
     public void updateOffset() {
-        offsetX = gamePanel.player.screenX - gamePanel.getPlayer().getX();
-        offsetY = gamePanel.player.screenY - gamePanel.getPlayer().getY();
+        offsetX = gamePanel.player.screenX - gamePanel.getPlayer().getSolidArea().x;
+        offsetY = gamePanel.player.screenY - gamePanel.getPlayer().getSolidArea().y;
     }
-    
+
     public int getOffsetX() {
         return offsetX;
     }
-    
+
     public int getOffsetY() {
         return offsetY;
     }
