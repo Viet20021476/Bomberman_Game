cedd4c9133dfec37a69acaf8a4d5d8911ea06eee
Viet20021476 <78094729+Viet20021476@users.noreply.github.com>
2021-12-11 10:32:10 +0700

    Update Entity.java

--- MyGame/src/Entities/Entity.java
+++ MyGame/src/Entities/Entity.java
@@ -13,62 +13,66 @@ public abstract class Entity {
     protected int spriteNum = 1;
     protected Rectangle solidArea;
     protected boolean collisionOn = false;
-    
+
     public abstract void update();
-    
+
     public abstract void draw(Graphics2D g2);
 
     public String getDirection() {
         return direction;
     }
-    
+
     public int getX() {
         return solidArea.x;
     }
-    
+
     public void setX(int x) {
         this.solidArea.x = x;
     }
-    
+
     public void setY(int y) {
         this.solidArea.y = y;
     }
-    
+
     public int getY() {
         return solidArea.y;
     }
-    
+
     public int getCenterX() {
         return solidArea.x + GamePanel.TILESIZE / 2;
     }
-    
+
     public int getCenterY() {
         return solidArea.y + GamePanel.TILESIZE / 2;
     }
-    
+
     public Rectangle getSolidArea() {
         return solidArea;
     }
-    
+
     public int getSpeed() {
         return speed;
     }
-    
+
     public boolean getCollisionOn() {
         return collisionOn;
     }
-    
+
     public void setCollision(boolean collisionOn) {
         this.collisionOn = collisionOn;
     }
-    
+
     protected int getSpriteNum() {
         return spriteNum;
     }
-    
+
     protected BufferedImage[] up = new BufferedImage[3];
     protected BufferedImage[] down = new BufferedImage[3];
     protected BufferedImage[] left = new BufferedImage[3];
     protected BufferedImage[] right = new BufferedImage[3];
     protected BufferedImage[] dead = new BufferedImage[4];
+
+    public BufferedImage[] getBufferedImageRight() {
+        return right;
+    }
 }
