76ae0703bb3b56427cddd58ffeb30825b00d2417
Viet20021476 <78094729+Viet20021476@users.noreply.github.com>
2021-12-11 10:30:21 +0700

    Update KeyHandle.java

--- MyGame/src/mygame/KeyHandle.java
+++ MyGame/src/mygame/KeyHandle.java
@@ -9,9 +9,9 @@ public class KeyHandle implements KeyListener {
     public boolean downPressed;
     public boolean leftPressed;
     public boolean rightPressed;
-    
+
     private GamePanel gamePanel;
-    
+
     public KeyHandle(GamePanel gamePanel) {
         super();
         this.gamePanel = gamePanel;
@@ -25,20 +25,48 @@ public class KeyHandle implements KeyListener {
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
 
-        if (code == KeyEvent.VK_W) {
-            upPressed = true;
+        if (gamePanel.gameState == gamePanel.titleState) {
+            if (code == KeyEvent.VK_W || code == KeyEvent.VK_UP) {
+                gamePanel.getScreenState().num--;
+                if (gamePanel.getScreenState().num < 0) {
+                    gamePanel.getScreenState().num = 1;
+                }
+            }
+
+            if (code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {
+                gamePanel.getScreenState().num++;
+                if (gamePanel.getScreenState().num > 1) {
+                    gamePanel.getScreenState().num = 0;
+                }
+            }
+
+            if (code == KeyEvent.VK_ENTER) {
+                if (gamePanel.getScreenState().num == 0) {
+                    gamePanel.gameState = gamePanel.playState;
+                    gamePanel.getSound().stop();
+                } else if (gamePanel.getScreenState().num == 1) {
+                    System.exit(0);
+                }
+            }
         }
-        if (code == KeyEvent.VK_S) {
-            downPressed = true;
-        }
-        if (code == KeyEvent.VK_A) {
-            leftPressed = true;
-        }
-        if (code == KeyEvent.VK_D) {
-            rightPressed = true;
-        }
-        if (code == KeyEvent.VK_J) {
-            gamePanel.getBombManager().addBomb();
+
+        if (gamePanel.gameState == gamePanel.playState) {
+
+            if (code == KeyEvent.VK_W) {
+                upPressed = true;
+            }
+            if (code == KeyEvent.VK_S) {
+                downPressed = true;
+            }
+            if (code == KeyEvent.VK_A) {
+                leftPressed = true;
+            }
+            if (code == KeyEvent.VK_D) {
+                rightPressed = true;
+            }
+            if (code == KeyEvent.VK_J) {
+                gamePanel.getBombManager().addBomb();
+            }
         }
     }
 
